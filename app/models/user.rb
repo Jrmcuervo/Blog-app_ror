bdefcaaf4f6ba424fb97ccab3e5ed2e513a1e46a
Jrmcuervo <jrmcuervom@gmail.com>
2023-08-16 15:29:35 -0600

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-    has_one_attached :photo
-    has_many :likes, foreign_key: 'author_id'
+  has_one_attached :photo
+  has_many :likes, foreign_key: 'author_id'
 end
-  
