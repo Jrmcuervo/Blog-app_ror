f2001b01262f6762f1e434becd5d89cf36bf50ae
Jrmcuervo <jrmcuervom@gmail.com>
2023-08-16 15:15:35 -0600

    Update files

--- db/migrate/20230816201915_create_comments.rb
+++ db/migrate/20230816201915_create_comments.rb
@@ -2,12 +2,11 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.integer :author_id, null: false
-      t.bigint :post_id, null: false
+      t.references :post, null: false, foreign_key: true
       t.text :text, null: false
 
       t.timestamps
     end
-    add_index :comments, :post_id
-    add_index :comments, :author_id
+    add_foreign_key :comments, :users, column: :author_id
   end
 end
--- db/migrate/20230816202012_create_likes.rb
+++ db/migrate/20230816202012_create_likes.rb
@@ -2,11 +2,10 @@ class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
       t.integer :author_id, null: false
-      t.bigint :post_id, null: false
+      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end
-    add_index :likes, :post_id
-    add_index :likes, :author_id
+    add_foreign_key :likes, :users, column: :author_id
   end
 end
