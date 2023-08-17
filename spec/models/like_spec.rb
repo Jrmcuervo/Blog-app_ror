c480a4f4ecbf06df4e0c0c629aa96a7497c2b54d
Jrmcuervo <jrmcuervom@gmail.com>
2023-08-17 15:02:19 -0600

    Added like model validations

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  let(:user) { User.create(name: 'Tom') }
+  let(:post) { Post.create(title: 'My First Post', text: 'This is the content of the post.', author: user) }
+
+
+  it 'is valid with an author and post' do
+    like = Like.new(author: user, post: post)
+    expect(like).to be_valid
+  end
+
+  it 'is not valid without an author' do
+    like = Like.new(author: nil, post: post)
+    expect(like).to_not be_valid
+  end
+
+  it 'is not valid without a post' do
+    like = Like.new(author: user, post: nil)
+    expect(like).to_not be_valid
+  end
+
+  describe 'callbacks' do
+    it 'increases the post likes_counter by 1 when created' do
+      expect { 
+        Like.create(author: user, post: post) 
+      }.to change { post.reload.likes_counter }.by(1)
+    end
+
+    it 'decreases the post likes_counter by 1 when destroyed' do
+      like = Like.create(author: user, post: post)
+      expect { 
+        like.destroy 
+      }.to change { post.reload.likes_counter }.by(-1)
+    end
+  end
+
+end
