864c3c4c9efa96b470d87a38718bb142451fa5c1
Jrmcuervo <jrmcuervom@gmail.com>
2023-08-24 13:52:41 -0600

    Fix linters

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/users"
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/users'
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:index)
-      expect(response.body).to include("Here is a list of all users")
+      expect(response.body).to include('Here is a list of all users')
     end
   end
 
-  describe "GET /show" do
-    let!(:user) { User.create(name: "Test User", photo: "URL", bio: "Bio") }
+  describe 'GET /show' do
+    let!(:user) { User.create(name: 'Test User', photo: 'URL', bio: 'Bio') }
 
-    it "returns http success" do
+    it 'returns http success' do
       get "/users/#{user.id}"
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:show)
-      expect(response.body).to include("Display single user details here")
+      expect(response.body).to include('Display single user details here')
     end
   end
 end
