fb41d7a53f69bd2515fb78a1eacff679bff830b3
Jrmcuervo <jrmcuervom@gmail.com>
2023-08-24 13:28:18 -0600

    Add tests for index and show actions in UsersController spec

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Users", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/users"
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+      expect(response.body).to include("Here is a list of all users")
+    end
+  end
+
+  describe "GET /show" do
+    let!(:user) { User.create(name: "Test User", photo: "URL", bio: "Bio") }
+
+    it "returns http success" do
+      get "/users/#{user.id}"
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      expect(response.body).to include("Display single user details here")
+    end
   end
 end
