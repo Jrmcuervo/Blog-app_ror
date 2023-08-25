10dabfa1bac507143d70ffb950bd2000a3f9643f
Jrmcuervo <jrmcuervom@gmail.com>
2023-08-25 17:13:38 -0600

    Update files

--- db/migrate/20230824220734_add_user_ref_to_likes.rb
+++
@@ -1,8 +0,0 @@
-class AddUserRefToLikes < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :posts, :user
-    Post.update_all(user_id: 1)
-    change_column_null :posts, :user_id, false
-  end
-  
-end
