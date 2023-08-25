1e5c4a16bb063304af3ccbcbf52a3c51642b24be
Jrmcuervo <jrmcuervom@gmail.com>
2023-08-25 15:25:45 -0600

    Fix issue to create new post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_24_220734) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_212057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,9 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_24_220734) do
     t.integer "likes_counter", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
