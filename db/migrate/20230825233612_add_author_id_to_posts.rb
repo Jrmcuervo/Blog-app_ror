c06badd9cca383240f948561e1d1cf89f0e0186a
Jrmcuervo <jrmcuervom@gmail.com>
2023-08-26 14:07:02 -0600

    Fix post creation and delete unnecessary migration files

--- db/migrate/20230825233612_add_author_id_to_posts.rb
+++
@@ -1,8 +0,0 @@
-class AddAuthorIdToPosts < ActiveRecord::Migration[7.0]
-  def change
-    Comment.delete_all
-    Post.delete_all
-    add_column :posts, :author_id, :integer, null: false
-  end
-end
-
