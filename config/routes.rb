19175d04b9e056a7d0deb5f7b2d94e139bbed1c1
Jrmcuervo <jrmcuervom@gmail.com>
2023-09-01 22:25:59 -0600

    Update related views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,16 @@ Rails.application.routes.draw do
     unlock: 'unblock'
   }
 
-  root to: 'devise/sessions#new'
+  devise_scope :user do
+    root to: 'devise/sessions#new', as: :login
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'secret', to: 'devise/passwords#new'
+    get 'unlock', to: 'devise/unlocks#new'
+    get 'verification', to: 'devise/confirmations#new'
+    get 'signup', to: 'devise/registrations#new'
+  end
 
+  root 'users#index'
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[new create]
